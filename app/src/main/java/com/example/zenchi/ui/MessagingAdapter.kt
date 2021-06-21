4ee834a1157a89ab15479daa23eb8240d648fc6c
Akansha Agarwal <akurox123@gmail.com>
2021-06-21 14:21:29 +0530

    implement Messaging Adapter logic + fix bugs

---
+++ app/src/main/java/com/example/zenchi/ui/MessagingAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.zenchi.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.zenchi.R
+import com.example.zenchi.data.Message
+import com.example.zenchi.utils.Constants.RECEIVE_ID
+import com.example.zenchi.utils.Constants.SEND_ID
+import kotlinx.android.synthetic.main.message_item.view.*
+
+class MessagingAdapter:RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
+
+    var messagesList=mutableListOf<Message>()
+
+
+    inner class MessageViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+        //delete message on double clicking it
+        init{
+            itemView.setOnClickListener{
+                messagesList.removeAt(adapterPosition)
+                notifyItemRemoved(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        return MessageViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_item,parent,false))
+    }
+
+    //Which side to display the messages - us(right->green), them(left->red)
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val currentMessage=messagesList[position]
+
+        when(currentMessage.id){
+            SEND_ID->{
+                holder.itemView.tv_message.apply{
+                    text=currentMessage.message
+                    visibility=View.VISIBLE
+                }
+                holder.itemView.tv_bot_message.visibility=View.GONE
+            }
+
+            RECEIVE_ID->{
+                holder.itemView.tv_bot_message.apply{
+                    text=currentMessage.message
+                    visibility=View.VISIBLE
+                }
+                holder.itemView.tv_message.visibility=View.GONE
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messagesList.size
+    }
+
+    fun insertMessage(message:Message){
+        this.messagesList.add(message)
+        notifyItemInserted(messagesList.size)
+    }
+}
