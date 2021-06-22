a98c12eae9650a2cbb9c79a291c1bcc0875ff9e4
Akansha Agarwal <akurox123@gmail.com>
2021-06-22 20:48:36 +0530

    Update MainActivity.kt (Glueing all functionalities)

--- app/src/main/java/com/example/zenchi/ui/MainActivity.kt
+++ app/src/main/java/com/example/zenchi/ui/MainActivity.kt
@@ -3,10 +3,144 @@ package com.example.zenchi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.zenchi.R
+import android.content.Intent
+import android.net.Uri
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.zenchi.data.Message
+import com.example.zenchi.utils.BotResponse
+import com.example.zenchi.utils.Constants.OPEN_GOOGLE
+import com.example.zenchi.utils.Constants.OPEN_SEARCH
+import com.example.zenchi.utils.Constants.RECEIVE_ID
+import com.example.zenchi.utils.Constants.SEND_ID
+import com.example.zenchi.utils.Time
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    var messagesList = mutableListOf<Message>()
+
+    private lateinit var adapter: MessagingAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView()
+
+        clickEvents()
+
+        //First message in Recylerview
+        customBotMessage("Hello! I am Zenchi!!! It means Omniscience that is all-knowing or one who has infinite knowledge in Japanese!!Anyways, how can I help you today??")
+    }
+
+    private fun clickEvents() {
+
+        //Send a message
+        btn_send.setOnClickListener {
+            sendMessage()
+        }
+
+        //Scroll back to correct position when user clicks on text view
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                }
+            }
+        }
+    }
+
+    private fun recyclerView() {
+        adapter = MessagingAdapter()    //instantiate adapter
+        rv_messages.adapter = adapter
+        rv_messages.layoutManager = LinearLayoutManager(applicationContext) //to make messages appear in a linear fashion as defined in Layouts by us
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //In case there are messages, scroll to bottom when re-opening app
+        GlobalScope.launch {
+            delay(100)
+            withContext(Dispatchers.Main) {
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
+    private fun sendMessage() {
+        val message = et_message.text.toString()        //et->edit text
+        val timeStamp = Time.timeStamp()
+
+        if (message.isNotEmpty()) {
+            //Adds it to our local list
+            messagesList.add(Message(message, SEND_ID, timeStamp))
+            et_message.setText("")
+
+            adapter.insertMessage(Message(message, SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount - 1)     //scroll to last mssg in UI
+
+
+            botResponse(message)
+        }
+    }
+
+    private fun botResponse(message: String) {
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch {
+            //Fake response delay
+            delay(1000)
+
+            withContext(Dispatchers.Main) {
+                //Gets the response
+                val response = BotResponse.basicResponses(message)
+
+                //Adds it to our local list
+                messagesList.add(Message(response, RECEIVE_ID, timeStamp))
+
+                //Inserts our message into the adapter
+                adapter.insertMessage(Message(response, RECEIVE_ID, timeStamp))
+
+                //Scrolls us to the position of the latest message
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                //Starts Google
+                when (response) {
+                    OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfterLast("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun customBotMessage(message: String) {
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+                val timeStamp = Time.timeStamp()
+                messagesList.add(Message(message, RECEIVE_ID, timeStamp))
+                adapter.insertMessage(Message(message, RECEIVE_ID, timeStamp))
+
+
+                //takes us to the last message
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
     }
 }
